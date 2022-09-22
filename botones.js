59e5d8bdf1a772db7c41145e700a7bec8ca24aa2
Kreisler Ramirez <itskreisler@outlook.com>
2022-09-22 14:18:41 -0500

    fix

--- botones.js
+++ botones.js
@@ -1,130 +1,119 @@
-            // Declaracion de variables
+// Declaracion de variables
 
-var dado = document.getElementById('dado');
-var dadoc1 = document.getElementById('dadoc1');
-var dadoc2 = document.getElementById('dadoc2');
+var dado = $("#dado"),
+  dadoc1 = $("#dadoc1"),
+  dadoc2 = $("#dadoc2");
 
-var c = document.getElementById('c');
-var c1 = document.getElementById('c1');
-var c2 = document.getElementById('c2');
+var c = $("#c"),
+  c1 = $("#c1"),
+  c2 = $("#c2");
 
-var valor1 = document.getElementById('valor1');
-var valor2 = document.getElementById('valor2');
+var valor1 = $("#valor1"),
+  valor2 = $("#valor2");
 
-var historial1 = document.getElementById('historial1');
-var historial2 = document.getElementById('historial2');
+var historial1 = $("#historial1"),
+  historial2 = $("#historial2");
 
-var h1scroll = document.getElementById('historial1');
-var h2scroll = document.getElementById('historial2');
+var h1scroll = $("#historial1"),
+  h2scroll = $("#historial2");
 
 var estado = 1;
 
+// El teteo
 
-            // El teteo
+var dembow = new Audio("audio/El Yala.mp3");
 
+on("click", $("#play"), () => {
+  dembow.play();
 
-var dembow = new Audio('audio/El Yala.mp3');
+  dado.style.transform = "rotateX(30deg) rotateY(-30deg)";
+  dadoc1.style.transform = "rotateX(-30deg) rotateY(30deg)";
+  dadoc2.style.transform = "rotateX(10deg) rotateZ(5deg)";
 
-play.addEventListener('click', ()=>{
-    dembow.play();
-
-    dado.style.transform = "rotateX(30deg) rotateY(-30deg)";
-    dadoc1.style.transform = "rotateX(-30deg) rotateY(30deg)";
-    dadoc2.style.transform = "rotateX(10deg) rotateZ(5deg)";
-    
-    dado.style.animation = "dembowyala 1.1s infinite 2.12s";
-    dadoc1.style.animation = "dembowyalac1 1.1s infinite 2.12s";
-    dadoc2.style.animation = "dembowyalac2 1.1s infinite 2.12s";
+  dado.style.animation = "dembowyala 1.1s infinite 2.12s";
+  dadoc1.style.animation = "dembowyalac1 1.1s infinite 2.12s";
+  dadoc2.style.animation = "dembowyalac2 1.1s infinite 2.12s";
 });
 
-pause.addEventListener('click', ()=>{
-    dembow.pause();
-    dembow.currentTime = 0;
+on("click", $("#pause"), () => {
+  dembow.pause();
+  dembow.currentTime = 0;
 
-    dado.style.transform = "rotate(0deg)";
-    dadoc1.style.transform = "rotate(0deg)";
-    dadoc2.style.transform = "rotate(0deg)";
+  dado.style.transform = "rotate(0deg)";
+  dadoc1.style.transform = "rotate(0deg)";
+  dadoc2.style.transform = "rotate(0deg)";
 
-    dado.style.animation = "dembowyalapausa 0s none";
-    dadoc1.style.animation = "dembowyalapausa 0s none";
-    dadoc2.style.animation = "dembowyalapausa 0s none";
+  dado.style.animation = "dembowyalapausa 0s none";
+  dadoc1.style.animation = "dembowyalapausa 0s none";
+  dadoc2.style.animation = "dembowyalapausa 0s none";
 });
 
+// Cambio de dados
 
+on("click", $("#unDado"), modo1);
 
-            // Cambio de dados 
+on("click", $("#dosDados"), modo2);
 
+function modo1() {
+  c.style.opacity = "1";
+  c1.style.opacity = "0";
+  c2.style.opacity = "0";
 
+  historial1.style.opacity = "1";
+  historial1.style.zIndex = "4";
 
-var unDado = document.getElementById('unDado');
-unDado.addEventListener('click', modo1);
+  historial2.style.opacity = "0";
+  historial2.style.zIndex = "1";
 
-var dosDados = document.getElementById('dosDados');
-dosDados.addEventListener('click', modo2);
-
-function modo1 (){
-    c.style.opacity = "1";
-    c1.style.opacity = "0";
-    c2.style.opacity = "0";
-
-    historial1.style.opacity = "1";
-    historial1.style.zIndex = "4";
-    
-    historial2.style.opacity = "0";
-    historial2.style.zIndex = "1";
-
-    estado = 1;
+  estado = 1;
 }
 
-function modo2 (){
-    c.style.opacity = "0";
-    c1.style.opacity = "1";
-    c2.style.opacity = "1";
-    
-    historial1.style.opacity = "0";
-    historial1.style.zIndex = "1";
+function modo2() {
+  c.style.opacity = "0";
+  c1.style.opacity = "1";
+  c2.style.opacity = "1";
 
-    historial2.style.opacity = "1";
-    historial2.style.zIndex = "4";
-    
-    estado = 2;
+  historial1.style.opacity = "0";
+  historial1.style.zIndex = "1";
+
+  historial2.style.opacity = "1";
+  historial2.style.zIndex = "4";
+
+  estado = 2;
 }
 
+// Numeros aleatorios
 
-            // Numeros aleatorios
+const random = [
+  "rotate(0deg)",
+  "rotateY(-90deg)",
+  "rotateY(90deg)",
+  "rotateX(90deg)",
+  "rotateX(-90deg)",
+  "rotateY(180deg)",
+];
 
+const randomNumberMax = (max) => Math.floor(Math.random() * max);
+const setRandom = () => {
+  var valorRandom = randomNumberMax(6),
+    valorRandomc1 = randomNumberMax(6),
+    valorRandomc2 = randomNumberMax(6);
 
+  dado.style.transform = random[valorRandom];
+  dadoc1.style.transform = random[valorRandomc1];
+  dadoc2.style.transform = random[valorRandomc2];
 
-const random = ["rotate(0deg)", "rotateY(-90deg)", "rotateY(90deg)", "rotateX(90deg)", "rotateX(-90deg)", "rotateY(180deg)"];
+  if (estado === 1) {
+    valor1.innerHTML += `${valorRandom + 1}<br><hr>`;
+    h1scroll.scrollTop = h1scroll.scrollHeight;
+  } else {
+    valor2.innerHTML += `${valorRandomc1 + valorRandomc2 + 2}<br><hr>`;
+    h2scroll.scrollTop = h2scroll.scrollHeight;
+  }
+};
+on("click", $("#randomBt"), setRandom);
+// Reiniciar
 
-var randomBt = document.getElementById('randomBt');
-randomBt.addEventListener('click', setRandom);
-
-function setRandom (){
-    var valorRandom = Math.floor(Math.random() * 6);
-    var valorRandomc1 = Math.floor(Math.random() * 6);
-    var valorRandomc2 = Math.floor(Math.random() * 6);
-
-    dado.style.transform = random[valorRandom];
-    dadoc1.style.transform = random[valorRandomc1];
-    dadoc2.style.transform = random[valorRandomc2];
-
-    if (estado == 1) {
-        valor1.innerHTML += valorRandom + 1 + "<br>" + "<hr>";
-        h1scroll.scrollTop = h1scroll.scrollHeight;
-    }else{
-        valor2.innerHTML += valorRandomc1 + valorRandomc2 + 2 + "<br>" + "<hr>"
-        h2scroll.scrollTop = h2scroll.scrollHeight;
-    }
-
-}
-
-
-            // Reiniciar
-
-            
-
-var reset = document.getElementById('reset');
-reset.addEventListener('click', ()=>{
-    window.location.reload();
-})
+on("click", $("#reset"), () => {
+  window.location.reload();
+});
--- dados.js
+++ dados.js
@@ -1,194 +1,192 @@
-            // Declarando variables de la botonera principal
+//Helpers
+/**
+ * @param  {string} e - selector
+ */
+/**
+ * @param {string} typeevent -
+ * blur Cuando el elemento pierde el foco.
+ * click El usuario hace clic sobre el elemento.
+ * dblclick El usuario hace doble clic sobre el elemento.
+ * focus El elemento gana el foco.
+ * keydown El usuario presiona una tecla.
+ * keypress El usuario presiona una tecla y la mantiene pulsada.
+ * keyup El usuario libera la tecla.
+ * load El documento termina su carga.
+ * mousedown El usuario presiona el botón del ratón en un elemento.
+ * mousemove El usuario mueve el puntero del ratón sobre un elemento.
+ * mouseout El usuario mueve el puntero fuera de un elemento.
+ * mouseover El usuario mantiene el puntero sobre un elemento.
+ * mouseup El usuario libera el botón pulsado del ratón sobre un elemento.
+ * unload El documento se descarga, bien porque se cierra la ventana, bien porque se navega a otra página.
+ * @param {String} el - elemento
+ * @param {void} callback - callback function
+ */
+const on = (typeevent, el, callback) => {
+  el.addEventListener(typeevent, (e) => callback(e));
+};
+const $ = (e) => document.querySelector(e);
+// Declarando variables de la botonera principal
 
-var dado = document.getElementById('dado');
-var dadoc1 = document.getElementById('dadoc1');
-var dadoc2 = document.getElementById('dadoc2');
+var dado = $("#dado"),
+  dadoc1 = $("#dadoc1"),
+  dadoc2 = $("#dadoc2");
 
-            // Agregando eventos de la botonera principal
+// Agregando eventos de la botonera principal
+[
+  ["click", "#lado1", clicked1a],
+  ["dblclick", "#lado1", clicked1a360],
+  ["click", "#lado2", clicked2a],
+  ["click", "#lado3", clicked3a],
+  ["click", "#lado4", clicked4a],
+  ["click", "#lado5", clicked5a],
+  ["click", "#lado6", clicked6a],
+].forEach(([evento, selector, fn]) => {
+  on(evento, $(selector), fn);
+});
 
-var cambio1 = document.getElementById('lado1');
-cambio1.addEventListener("click", clicked1a);
-var cambio1 = document.getElementById('lado1');
-cambio1.addEventListener("dblclick", clicked1a360);
+// Creando las funciones de la botonera principal
 
-var cambio2 = document.getElementById('lado2');
-cambio2.addEventListener("click", clicked2a);
-
-var cambio3 = document.getElementById('lado3');
-cambio3.addEventListener("click", clicked3a);
-
-var cambio4 = document.getElementById('lado4');
-cambio4.addEventListener("click", clicked4a);
-
-var cambio5 = document.getElementById('lado5');
-cambio5.addEventListener("click", clicked5a);
-
-var cambio6 = document.getElementById('lado6');
-cambio6.addEventListener("click", clicked6a);
-
-            // Creando las funciones de la botonera principal
-
-function clicked1a(){
-    dado.style.transform = "rotate(0deg)";
-    dadoc1.style.transform = "rotate(0deg)";
+function clicked1a() {
+  dado.style.transform = "rotate(0deg)";
+  dadoc1.style.transform = "rotate(0deg)";
 }
-function clicked1a360(){
-    dado.style.transform = "rotatez(180deg)";
-    dadoc1.style.transform = "rotatez(180deg)";
-}
-
-function clicked2a(){
-    dado.style.transform = "rotateY(-90deg)";
-    dadoc1.style.transform = "rotateY(-90deg)";
-}
-
-function clicked3a(){
-    dado.style.transform = "rotateY(90deg)";
-    dadoc1.style.transform = "rotateY(90deg)";
-}
-
-function clicked4a(){
-    dado.style.transform = "rotateX(90deg)";
-    dadoc1.style.transform = "rotateX(90deg)";
-}
-
-function clicked5a(){
-    dado.style.transform = "rotateX(-90deg)";
-    dadoc1.style.transform = "rotateX(-90deg)";
+function clicked1a360() {
+  dado.style.transform = "rotatez(180deg)";
+  dadoc1.style.transform = "rotatez(180deg)";
 }
 
-function clicked6a(){
-    dado.style.transform = "rotateY(180deg)";
-    dadoc1.style.transform = "rotateY(180deg)";
+function clicked2a() {
+  dado.style.transform = "rotateY(-90deg)";
+  dadoc1.style.transform = "rotateY(-90deg)";
+}
+
+function clicked3a() {
+  dado.style.transform = "rotateY(90deg)";
+  dadoc1.style.transform = "rotateY(90deg)";
+}
+
+function clicked4a() {
+  dado.style.transform = "rotateX(90deg)";
+  dadoc1.style.transform = "rotateX(90deg)";
+}
+
+function clicked5a() {
+  dado.style.transform = "rotateX(-90deg)";
+  dadoc1.style.transform = "rotateX(-90deg)";
+}
+
+function clicked6a() {
+  dado.style.transform = "rotateY(180deg)";
+  dadoc1.style.transform = "rotateY(180deg)";
 }
 
 // Agregando eventos de la botonera secundaria para el dado grande (mouseover/mouseleave) y el pequeño (click)
+[
+  ["mouseover", hover1a],
+  ["mouseleave", hover1b],
+  ["click", clicked1b],
+  ["dblclick", clicked1b360],
+].forEach(([evento, fn]) => {
+  on(evento, $("#lado1h"), fn);
+});
 
-var cambio1 = document.getElementById('lado1h');
-cambio1.addEventListener("mouseover", hover1a);
-var cambio1 = document.getElementById('lado1h');
-cambio1.addEventListener("mouseleave", hover1b);
+on("mouseover", $("#lado2h"), hover2a);
 
-var cambio1 = document.getElementById('lado1h');
-cambio1.addEventListener("click", clicked1b);
-var cambio1 = document.getElementById('lado1h');
-cambio1.addEventListener("dblclick", clicked1b360);
+on("mouseleave", $("#lado2h"), hover2b);
 
+on("click", $("#lado2h"), clicked2b);
 
-var cambio2 = document.getElementById('lado2h');
-cambio2.addEventListener("mouseover", hover2a);
-var cambio2 = document.getElementById('lado2h');
-cambio2.addEventListener("mouseleave", hover2b);
+on("mouseover", $("#lado3h"), hover3a);
 
-var cambio2 = document.getElementById('lado2h');
-cambio2.addEventListener("click", clicked2b);
+on("mouseleave", $("#lado3h"), hover3b);
 
+on("click", $("#lado3h"), clicked3b);
 
-var cambio3 = document.getElementById('lado3h');
-cambio3.addEventListener("mouseover", hover3a);
-var cambio3 = document.getElementById('lado3h');
-cambio3.addEventListener("mouseleave", hover3b);
+on("mouseover", $("#lado4h"), hover4a);
 
-var cambio3 = document.getElementById('lado3h');
-cambio3.addEventListener("click", clicked3b);
+on("mouseleave", $("#lado4h"), hover4b);
 
+on("click", $("#lado4h"), clicked4b);
 
-var cambio4 = document.getElementById('lado4h');
-cambio4.addEventListener("mouseover", hover4a);
-var cambio4 = document.getElementById('lado4h');
-cambio4.addEventListener("mouseleave", hover4b);
+on("mouseover", $("#lado5h"), hover5a);
 
-var cambio4 = document.getElementById('lado4h');
-cambio4.addEventListener("click", clicked4b);
+on("mouseleave", $("#lado5h"), hover5b);
 
+on("click", $("#lado5h"), clicked5b);
 
-var cambio5 = document.getElementById('lado5h');
-cambio5.addEventListener("mouseover", hover5a);
-var cambio5 = document.getElementById('lado5h');
-cambio5.addEventListener("mouseleave", hover5b);
+on("mouseover", $("#lado6h"), hover6a);
 
-var cambio5 = document.getElementById('lado5h');
-cambio5.addEventListener("click", clicked5b);
+on("mouseleave", $("#lado6h"), hover6b);
 
-
-var cambio6 = document.getElementById('lado6h');
-cambio6.addEventListener("mouseover", hover6a);
-var cambio6 = document.getElementById('lado6h');
-cambio6.addEventListener("mouseleave", hover6b);
-
-var cambio6 = document.getElementById('lado6h');
-cambio6.addEventListener("click", clicked6b);
+on("click", $("#lado6h"), clicked6b);
 
 // Creando las funciones de la botonera secundaria para el dado grande (mouseover/mouseleave) y el pequeño (click)
 
-function hover1a(){
-    dado.style.transform = "rotate(0deg)";
-    dado.style.transform = "scale(105%)";
+function hover1a() {
+  //dado.style.transform = "rotate(0deg)";
 }
-function hover1b(){
-    dado.style.transform = "rotate(0deg)";
-}
-
-function hover2a(){
-    dado.style.transform = "rotateY(-90deg)";
-}
-function hover2b(){
-    dado.style.transform = "rotateY(0deg)";
+function hover1b() {
+  dado.style.transform = "rotate(0deg)";
 }
 
-function hover3a(){
-    dado.style.transform = "rotateY(90deg)";
+function hover2a() {
+  dado.style.transform = "rotateY(-90deg)";
 }
-function hover3b(){
-    dado.style.transform = "rotateY(0deg)";
-}
-
-function hover4a(){
-    dado.style.transform = "rotateX(90deg)";
-}
-function hover4b(){
-    dado.style.transform = "rotateX(0deg)";
+function hover2b() {
+  dado.style.transform = "rotateY(0deg)";
 }
 
-function hover5a(){
-    dado.style.transform = "rotateX(-90deg)";
+function hover3a() {
+  dado.style.transform = "rotateY(90deg)";
 }
-function hover5b(){
-    dado.style.transform = "rotateX(0deg)";
-}
-
-function hover6a(){
-    dado.style.transform = "rotateY(180deg)";
-}
-function hover6b(){
-    dado.style.transform = "rotateY(0deg)";
+function hover3b() {
+  dado.style.transform = "rotateY(0deg)";
 }
 
-
-
-function clicked1b(){
-    dadoc2.style.transform = "rotate(0deg)";
+function hover4a() {
+  dado.style.transform = "rotateX(90deg)";
 }
-function clicked1b360(){
-    dadoc2.style.transform = "rotatez(180deg)";
+function hover4b() {
+  dado.style.transform = "rotateX(0deg)";
+}
+
+function hover5a() {
+  dado.style.transform = "rotateX(-90deg)";
+}
+function hover5b() {
+  dado.style.transform = "rotateX(0deg)";
+}
+
+function hover6a() {
+  dado.style.transform = "rotateY(180deg)";
+}
+function hover6b() {
+  dado.style.transform = "rotateY(0deg)";
+}
+
+function clicked1b() {
+  dadoc2.style.transform = "rotate(0deg)";
+}
+function clicked1b360() {
+  dadoc2.style.transform = "rotatez(180deg)";
 }
 
 function clicked2b() {
-    dadoc2.style.transform = "rotateY(-90deg)";
+  dadoc2.style.transform = "rotateY(-90deg)";
 }
 
 function clicked3b() {
-    dadoc2.style.transform = "rotateY(90deg)";
+  dadoc2.style.transform = "rotateY(90deg)";
 }
 
 function clicked4b() {
-    dadoc2.style.transform = "rotateX(90deg)";
+  dadoc2.style.transform = "rotateX(90deg)";
 }
 
 function clicked5b() {
-    dadoc2.style.transform = "rotateX(-90deg)";
+  dadoc2.style.transform = "rotateX(-90deg)";
 }
 
 function clicked6b() {
-    dadoc2.style.transform = "rotateY(180deg)";
+  dadoc2.style.transform = "rotateY(180deg)";
 }
